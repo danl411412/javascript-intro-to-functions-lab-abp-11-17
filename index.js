dd71071ac9944db925392264b67cbc6149dbac36
danl411412 <daan.b.lee@gmail.com>
2017-11-16 07:09:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,13 @@ function logShout(str) {
 function logWhisper(str) {
   console.log(str.toLowerCase());
 }
+
+function sayHiToGrandma(str) {
+  if(str === str.toUpperCase()) {
+    return "YES INDEED!";
+  } else if(str === str.toLowerCase()) {
+    return "I can\'t hear you!";
+  } else if (str === "I love you, Grandma."){
+    return "I love you, too.";
+  }
+}
